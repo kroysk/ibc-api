cc1f2250f0f2a0e031ac873d51dc0e2aa5cb7a33
Jorge Diaz <kroycuentas@gmail.com>
2025-08-01 09:37:20 -0500

    qsas

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-# rails db:create RAILS_ENV=production
+rails db:create RAILS_ENV=production
 rails db:migrate RAILS_ENV=production
 rails db:seed  RAILS_ENV=production
